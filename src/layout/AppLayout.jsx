746e3049279596849cff338c546e7472d6c75541
Sergio Duran <538ser@gmail.com>
2024-12-07 10:21:55 -0600

    form

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,12 +1,14 @@
-import { Navbar } from "../components"
-import { Footer } from "../ui"
+import { Navbar } from "../components";
+import { Footer } from "../ui";
 
 export const AppLayout = ({ children }) => {
   return (
     <>
+      <div className="flex flex-col justify-between min-h-[100vh]">
         <Navbar />
-        <main>{ children }</main>
+        <main>{children}</main>
         <Footer />
+      </div>
     </>
-  )
-}
+  );
+};
--- src/ui/ContactForm.jsx
+++ src/ui/ContactForm.jsx
@@ -1,26 +1,70 @@
+import { BsFillSendArrowUpFill } from "react-icons/bs";
+
+
 export const ContactForm = () => {
   return (
-    <form className="max-w-md mx-auto">
-      <div className="flex flex-col mb-6">
-        <label htmlFor="name" className="mb-2 font-bold">
-          Name
+    <form className="max-w-2xl mx-auto mb-28">
+      <div className="grid md:grid-cols-2 md:gap-6">
+        <div className="relative z-0 w-full mb-5 group">
+          <input
+            type="text"
+            name="floating_first_name"
+            id="floating_first_name"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-primary focus:outline-none focus:ring-0 focus:border-primary peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="floating_first_name"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-primary peer-focus:dark:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Name
+          </label>
+        </div>
+        <div className="relative z-0 w-full mb-5 group">
+          <input
+            type="email"
+            name="floating_last_name"
+            id="floating_last_name"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-primary focus:outline-none focus:ring-0 focus:border-primary peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="floating_last_name"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-primary peer-focus:dark:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Email
+          </label>
+        </div>
+      </div>
+      <div className="relative z-0 w-full mb-5 group">
+        <textarea
+          name="floating_message"
+          id="floating_message"
+          rows="4"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-primary focus:outline-none focus:ring-0 focus:border-primary peer"
+          placeholder=" "
+          required
+        ></textarea>
+        <label
+          htmlFor="floating_message"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-primary peer-focus:dark:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          Your Message
         </label>
-        <input type="text" id="name" className="outline outline-1 rounded-lg min-h-11 outline-slate-300" />
       </div>
 
-      <div className="flex flex-col mb-6">
-        <label htmlFor="email" className="mb-2 font-bold">Email</label>
-        <input type="email" id="email" className="outline outline-1 rounded-lg min-h-11 outline-slate-300" />
-      </div>
+      <div className="flex justify-center">
+        <button
+          type="submit"
+          className=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-1/2 px-5 py-2.5 text-center dark:bg-primary dark:hover:bg-primary/95 dark:focus:ring-blue-800 mt-8"
+        >
+        <div className="flex items-center justify-center gap-2">        <p>Submit</p>
+        <BsFillSendArrowUpFill /></div>
 
-      <div className="flex flex-col mb-6">
-        <label htmlFor="message" className="mb-2 font-bold">
-          Message
-        </label>
-        <textarea id="message" rows={5} className="outline outline-1 rounded-lg outline-slate-300"></textarea>
+        </button>
       </div>
-
-      <button className="bg-primary px-6 py-2 rounded-lg font-bold text-white">Send</button>
     </form>
   );
 };
