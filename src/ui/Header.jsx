678c7a8e91996bf1a806b8b7c3120ec0d72dccfb
Sergio Duran <538ser@gmail.com>
2024-12-10 19:10:49 -0600

    header changes

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -6,15 +6,45 @@ export const Header = () => {
     <header className="flex items-center mt-40 justify-center mb-48">
       {/* Content */}
       <div className="space-y-10 text-center flex flex-col items-center">
-        <h1 className="leading-[80px] text-white">
-          Hello, my name is <span className="text-primary">Sergio Duran.</span>
-        </h1>
+        <div className="flex items-center gap-8">
+          <div>
+            <h1 className="text-white">
+              Hello, I'm <span className="text-primary">Sergio Duran</span>
+            </h1>
+          </div>
+          <div>
+            <div className="relative w-20 h-20">
+              {/* BG Image */}
+              <img
+                src="selfie-bg.svg"
+                alt="Background Color"
+                className="absolute top-0 left-0 w-full h-full"
+              />
+
+              {/* Selfie */}
+              <img
+                src="selfie.webp"
+                alt="Selfie Image"
+                className="absolute top-0 left-0 w-full h-full"
+                style={{
+                  WebkitMaskImage: "url('selfie-bg.svg')",
+                  WebkitMaskRepeat: "no-repeat",
+                  WebkitMaskSize: "cover",
+                  maskImage: "url('selfie-bg.svg')",
+                  maskRepeat: "no-repeat",
+                  maskSize: "cover",
+                }}
+              />
+            </div>
+          </div>
+        </div>
         <p className="text-white text-xl max-w-md">
           I'm a{" "}
           <span className="text-primary font-bold text-xl">
             software developer
           </span>{" "}
-          from San Jose, Costa Rica, who likes to build web applications and learn new things everyday!
+          from San Jose, Costa Rica, who likes to build web applications and
+          learn new things everyday!
         </p>
 
         <div className="flex gap-4 justify-center">
