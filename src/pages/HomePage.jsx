0c9739c6d9c5343d86cf2f2da46e0ac293bda5b8
Sergio Duran <538ser@gmail.com>
2024-12-12 18:26:31 -0600

    added parragraphs to the about me section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -20,22 +20,40 @@ export const HomePage = () => {
       <div className="flex items-center max-w-3xl mx-auto">
         <div className="space-y-4 max-w-md">
           <p className="text-sm leading-relaxed text-terciary">
-            I am a software developer with a strong focus on frontend
-            technologies. I am passionate about writing clean, maintainable code
-            and staying up-to-date with the latest trends in frontend
-            development.
+            Hello, my name is Sergio, and I am a{" "}
+            <span className="text-primary">software developer</span>{" "}
+            specializing in{" "}
+            <span className="text-primary">Frontend development</span>. While my
+            academic background is in agronomy, it was during my research
+            projects, where I used tools like{" "}
+            <span className="text-primary">R</span> and{" "}
+            <span className="text-primary">Python</span> for {''}
+            <span className="text-primary">statistical analysis</span>, that I
+            discovered my interest in programming.
           </p>
           <p className="text-sm leading-relaxed text-terciary">
-            I am a software developer with a strong focus on frontend
-            technologies. I am passionate about writing clean, maintainable code
-            and staying up-to-date with the latest trends in frontend
-            development.
+            These early experiences with{" "}
+            <span className="text-primary">script development</span> and {''}
+            <span className="text-primary">process automation</span> sparked my
+            curiosity about the world of {''}
+            <span className="text-primary">software development</span>, leading
+            me to explore and fully dedicate myself to this exciting career.
           </p>
           <p className="text-sm leading-relaxed text-terciary">
-            I am a software developer with a strong focus on frontend
-            technologies. I am passionate about writing clean, maintainable code
-            and staying up-to-date with the latest trends in frontend
-            development.
+            I am currently seeking my first professional opportunity in{" "}
+            <span className="text-primary">software development</span>. I am
+            eager to join a team where I can contribute to creating{" "}
+            <span className="text-primary">user-centric web applications</span>{" "}
+            while continuing to grow as a developer.
+          </p>
+
+          <p className="text-sm leading-relaxed text-terciary">
+            In the meantime, I dedicate my time to creating{" "}
+            <span className="text-primary">personal projects</span> that allow
+            me to apply and strengthen my knowledge, while also learning new{" "}
+            <span className="text-primary">technologies</span> and{" "}
+            <span className="text-primary">tools</span> to stay up-to-date in
+            the field.
           </p>
         </div>
 
