bb3f86b8d677f8455309f2dbcbb419a99099266c
Sergio Duran <78579628+tutaabsoluta@users.noreply.github.com>
2025-03-27 21:13:06 -0600

    Actualizar AboutContent.jsx

--- src/components/about/AboutContent.jsx
+++ src/components/about/AboutContent.jsx
@@ -5,13 +5,12 @@ export const AboutContent = () => {
         I’m a <span className="text-primary">web developer</span> with experience in 
         <span className="text-primary"> full-stack development</span> using JavaScript technologies. 
         Although my background is in agronomy, working with R and Python for statistical analysis 
-        led me to discover my passion for programming, pushing me to transition into software development.
+        led me to discover the world of programming, pushing me to transition into software development.
       </p>
       <p className="text-md leading-relaxed text-terciary">
-        I’m currently looking for my first professional opportunity as a 
-        <span className="text-primary"> web developer</span>. I’m passionate about building 
-        <span className="text-primary"> high-performance applications</span>, always striving 
-        to write clean, maintainable code while keeping up with emerging technologies.
+        Currently seeking for my first professional opportunity as a 
+        <span className="text-primary"> web developer</span>, with a strong drive about building 
+        <span className="text-primary"> high-performance applications</span>. Focused on writing clean, maintainable code while keeping up with emerging technologies.
       </p>
       <p className="text-md leading-relaxed text-terciary">
         Meanwhile, I dedicate my time to personal projects and 
