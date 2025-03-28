55ed13faec0debc6e0ede00e71129adf9660e7ea
Sergio Duran <538ser@gmail.com>
2025-03-28 11:56:02 -0600

    Navbar changes

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -2,16 +2,18 @@ import { BurgerMenu } from "./BurgerMenu";
 
 export const Navbar = () => {
   return (
-    <div 
-      className="w-full mx-auto p-5 relative z-20"
-    >
+    <div className="w-full mx-auto p-5 relative z-20">
       <div className="flex justify-between items-center">
         <div>
-          <img src="logo.webp" alt="" className="h-20 w-full" />
+          <img
+            src="logo.webp"
+            alt="Logo"
+            className="h-20 w-auto"
+          />
         </div>
 
         <div className="fixed top-10 right-2 md:right-6">
-         <BurgerMenu />
+          <BurgerMenu />
         </div>
       </div>
     </div>
